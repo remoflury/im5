3336c8688b1594c1ca901f14c96055b2cfce2c89
remoflury <remoflury97+web@gmail.com>
2023-12-06 09:59:29 +0100

    page transition

--- src/routes/(loggedIn)/+layout.svelte
+++ src/routes/(loggedIn)/+layout.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
 	import NavComponent from '$lib/components/navComponent.svelte';
 	import { page } from '$app/stores';
+	import { fly } from 'svelte/transition';
+	import { cubicOut } from 'svelte/easing';
 </script>
 
 {#if $page.data.session}
 	<NavComponent />
 {/if}
-<slot />
+
+{#key $page.url.pathname}
+	<div
+		class="logged-in-container"
+		in:fly={{ y: 10, duration: 200, delay: 400, easing: cubicOut }}
+		out:fly={{ y: 10, duration: 200, easing: cubicOut }}
+	>
+		<slot />
+	</div>
+{/key}
